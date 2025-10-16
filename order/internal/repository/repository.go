9e211c0fd6e080f6975bcdb9ed4f0ef77fadcf1e
Alexandr <alexandr@biznes-len.com>
2025-10-16 17:29:49 +0300

    feat: Добавлены репо и сервис интерфейсы

--- order/internal/repository/repository.go
+++ order/internal/repository/repository.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+	"github.com/google/uuid"
+)
+
+type OrderRepository interface {
+	Create() error
+	Get(id uuid.UUID) error
+	Update(id uuid.UUID) error
+}
--- order/internal/service/service.go
+++ order/internal/service/service.go
@@ -1,1 +1,10 @@
 package service
+
+import "github.com/google/uuid"
+
+type OrderService interface {
+	Get(orderID uuid.UUID) (string, error)
+	Create(userID uuid.UUID, parts []uuid.UUID) (string, error)
+	Cancel(orderID uuid.UUID) error
+	Pay(orderID uuid.UUID) (string, error)
+}
