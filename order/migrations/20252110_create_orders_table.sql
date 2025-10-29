4a597789520e5fe989bbe8f4966ce0b74e590c0b
Alexandr <alexandr@biznes-len.com>
2025-10-29 13:02:39 +0300

    fix: Исправлена миграция

--- order/migrations/20252110_create_orders_table.sql
+++ order/migrations/20252110_create_orders_table.sql
@@ -1,17 +1,27 @@
 -- +goose Up
+
+-- создаем таблицу заказов
 CREATE TABLE orders (
     order_uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_uuid UUID NOT NULL,
     part_uuids UUID[] NOT NULL DEFAULT '{}',
     total_price NUMERIC(10,2) NOT NULL CHECK (total_price >= 0),
     transaction_uuid UUID,
-    payment_method VARCHAR(30) NOT NULL
-        CHECK (payment_method IN ('UNKNOWN', 'CARD', 'SBP', 'CREDIT_CARD', 'INVESTOR_MONEY')) DEFAULT 'UNKNOWN',
-    status VARCHAR(30) NOT NULL
-        CHECK (status IN ('PENDING_PAYMENT', 'PAID', 'CANCELLED')) DEFAULT 'PENDING_PAYMENT',
-    created_at TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP
+    payment_method VARCHAR(30) NOT NULL DEFAULT 'UNKNOWN',
+    status VARCHAR(30) NOT NULL DEFAULT 'PENDING_PAYMENT',
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE
 );
+
+-- создаем индекс по user_uuid для быстрого поиска заказов пользователя
+CREATE INDEX IF NOT EXISTS idx_orders_user_uuid ON orders (user_uuid);
+
 
 -- +goose Down
 DROP TABLE orders
+
+-- удаляем таблицу заказов
+DROP TABLE IF EXISTS orders;
+
+-- удаляем индекс по user_uuid
+DROP INDEX IF EXISTS idx_orders_user_uuid;
