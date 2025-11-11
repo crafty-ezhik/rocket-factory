02500cd404741178f46bdd3595cb182884fcc662
Alexandr Kotov <sa.cotov@yandex.ru>
2025-11-12 02:37:21 +0300

    feat: Реализована логика Тг в сервисном слое

---
+++ notification/internal/service/telegram/service.go
@@ -0,0 +1,106 @@
+package telegram
+
+import (
+	"bytes"
+	"context"
+	"embed"
+	"text/template"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/crafty-ezhik/rocket-factory/notification/internal/client/http"
+	"github.com/crafty-ezhik/rocket-factory/notification/internal/model"
+	"github.com/crafty-ezhik/rocket-factory/platform/pkg/logger"
+)
+
+const chatID = 12345678
+
+//go:embed templates/order_paid_notification.tmpl
+//go:embed templates/order_assembled_notification.tmpl
+var templateFS embed.FS
+
+type orderPaidTemplateData struct {
+	OrderUUID       string
+	TransactionUUID string
+	PaymentMethod   string
+	PaymentDate     string
+}
+
+type orderAssembledTemplateData struct {
+	OrderUUID    string
+	BuildTimeSec int
+}
+
+var (
+	orderPaidTemplate      = template.Must(template.ParseFS(templateFS, "order_paid_notification.tmpl"))
+	orderAssembledTemplate = template.Must(template.ParseFS(templateFS, "order_assembled_notification.tmpl"))
+)
+
+type service struct {
+	tgClient http.TelegramClient
+}
+
+func NewService(tgClient http.TelegramClient) *service {
+	return &service{
+		tgClient: tgClient,
+	}
+}
+
+func (s *service) SendOrderPaidNotification(ctx context.Context, msg model.OrderPaidEvent) error {
+	message, err := s.buildPaidMsg(msg)
+	if err != nil {
+		return err
+	}
+
+	err = s.tgClient.SendMessage(ctx, chatID, message)
+	if err != nil {
+		return err
+	}
+	logger.Info(ctx, "Telegram message sent to chat", zap.Int("chat_id", chatID), zap.String("message", message))
+	return nil
+}
+
+func (s *service) SendOrderAssembledNotification(ctx context.Context, msg model.OrderAssembledEvent) error {
+	message, err := s.buildAssembledMsg(msg)
+	if err != nil {
+		return err
+	}
+	err = s.tgClient.SendMessage(ctx, chatID, message)
+	if err != nil {
+		return err
+	}
+
+	logger.Info(ctx, "Telegram message sent to chat", zap.Int("chat_id", chatID), zap.String("message", message))
+	return nil
+}
+
+func (s *service) buildPaidMsg(msg model.OrderPaidEvent) (string, error) {
+	data := orderPaidTemplateData{
+		OrderUUID:       msg.OrderUUID.String(),
+		TransactionUUID: msg.TransactionUUID.String(),
+		PaymentMethod:   msg.PaymentMethod,
+		PaymentDate:     time.Now().Format(time.DateTime),
+	}
+
+	var buf bytes.Buffer
+	err := orderPaidTemplate.Execute(&buf, data)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func (s *service) buildAssembledMsg(msg model.OrderAssembledEvent) (string, error) {
+	data := orderAssembledTemplateData{
+		OrderUUID:    msg.OrderUUID.String(),
+		BuildTimeSec: msg.BuildTimeSec,
+	}
+
+	var buf bytes.Buffer
+	err := orderAssembledTemplate.Execute(&buf, data)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
