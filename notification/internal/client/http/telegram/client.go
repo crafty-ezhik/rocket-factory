cfce499d7163cb74b4b38d0f0a05685cb138fdbd
Alexandr Kotov <sa.cotov@yandex.ru>
2025-11-12 02:03:29 +0300

    feat: добавлен ТГ клиент

---
+++ notification/internal/client/http/telegram/client.go
@@ -0,0 +1,28 @@
+package telegram
+
+import (
+	"context"
+	"github.com/go-telegram/bot"
+)
+
+type client struct {
+	bot *bot.Bot
+}
+
+func NewClient(bot *bot.Bot) *client {
+	return &client{
+		bot: bot,
+	}
+}
+
+func (c *client) SendMessage(ctx context.Context, chatID int64, message string) error {
+	_, err := c.bot.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:    chatID,
+		Text:      message,
+		ParseMode: "Markdown",
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
