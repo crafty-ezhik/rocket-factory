601eac3da120d8df32d946deee0761041df2dbcd
Alexandr <alexandr@biznes-len.com>
2025-11-19 17:54:50 +0300

    feat: Добавлен мигратор и файлы миграции

---
+++ iam/migrations/1_create_users_table.up.sql
@@ -0,0 +1,23 @@
+-- Создаем таблицу с пользователями
+create table users (
+    user_uuid uuid primary key default gen_random_uuid(),
+    login varchar(100) not null ,
+    email varchar(100) not null ,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE
+);
+
+-- Создаем индекс по user_uuid для быстрого поиска пользователей
+CREATE INDEX IF NOT EXISTS idx_iam_user_uuid ON users (user_uuid);
+
+-- Создаем таблицу с методами нотификации
+create table notification_methods (
+    id serial primary key,
+    user_uuid UUID not null references users(user_uuid) on delete cascade ,
+    provider_name text not null ,
+    target text not null
+);
+
+
+
+
