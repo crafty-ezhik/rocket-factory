19c35fe85bfc4fc0551e7aaca284c47768a066d1
Alexandr <alexandr@biznes-len.com>
2025-11-21 14:55:48 +0300

    feat: Добавлены моки для IAM

--- notification/internal/config/mocks/mock_telegram_bot_config.go
+++ notification/internal/config/mocks/mock_telegram_bot_config.go
@@ -38,6 +38,50 @@ func (_m *MockTelegramBotConfig) EXPECT() *MockTelegramBotConfig_Expecter {
 	return &MockTelegramBotConfig_Expecter{mock: &_m.Mock}
 }
 
+// ChatID provides a mock function for the type MockTelegramBotConfig
+func (_mock *MockTelegramBotConfig) ChatID() int64 {
+	ret := _mock.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for ChatID")
+	}
+
+	var r0 int64
+	if returnFunc, ok := ret.Get(0).(func() int64); ok {
+		r0 = returnFunc()
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+	return r0
+}
+
+// MockTelegramBotConfig_ChatID_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ChatID'
+type MockTelegramBotConfig_ChatID_Call struct {
+	*mock.Call
+}
+
+// ChatID is a helper method to define mock.On call
+func (_e *MockTelegramBotConfig_Expecter) ChatID() *MockTelegramBotConfig_ChatID_Call {
+	return &MockTelegramBotConfig_ChatID_Call{Call: _e.mock.On("ChatID")}
+}
+
+func (_c *MockTelegramBotConfig_ChatID_Call) Run(run func()) *MockTelegramBotConfig_ChatID_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *MockTelegramBotConfig_ChatID_Call) Return(n int64) *MockTelegramBotConfig_ChatID_Call {
+	_c.Call.Return(n)
+	return _c
+}
+
+func (_c *MockTelegramBotConfig_ChatID_Call) RunAndReturn(run func() int64) *MockTelegramBotConfig_ChatID_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // Token provides a mock function for the type MockTelegramBotConfig
 func (_mock *MockTelegramBotConfig) Token() string {
 	ret := _mock.Called()
