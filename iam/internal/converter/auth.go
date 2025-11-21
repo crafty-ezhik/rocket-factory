60f8128f03721bfed3bc634f02247232ad8609e0
Alexandr <alexandr@biznes-len.com>
2025-11-21 14:41:03 +0300

    feat: Добавлены конверторы

--- iam/internal/converter/auth.go
+++ iam/internal/converter/auth.go
@@ -1,1 +1,29 @@
 package converter
+
+import (
+	"github.com/crafty-ezhik/rocket-factory/iam/internal/model"
+	authV1 "github.com/crafty-ezhik/rocket-factory/shared/pkg/proto/auth/v1"
+	commonV1 "github.com/crafty-ezhik/rocket-factory/shared/pkg/proto/common/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func WhoamiResponseToProto(data model.WhoamiResponse, sessionUUID string) *authV1.WhoamiResponse {
+	return &authV1.WhoamiResponse{
+		Session: SessionToProto(data.Session, sessionUUID),
+		User:    UserToProto(data.User),
+	}
+}
+
+func SessionToProto(data model.Session, sessionUUID string) *commonV1.Session {
+	var updatedAt *timestamppb.Timestamp
+	if data.UpdatedAt != nil {
+		updatedAt = timestamppb.New(*data.UpdatedAt)
+	}
+
+	return &commonV1.Session{
+		Uuid:      sessionUUID,
+		CreatedAt: timestamppb.New(data.CreatedAt),
+		UpdatedAt: updatedAt,
+		ExpiresAt: timestamppb.New(data.ExpiresAt),
+	}
+}
--- iam/internal/repository/converter/session.go
+++ iam/internal/repository/converter/session.go
@@ -1,1 +1,23 @@
 package converter
+
+import (
+	serviceModel "github.com/crafty-ezhik/rocket-factory/iam/internal/model"
+	repoModel "github.com/crafty-ezhik/rocket-factory/iam/internal/repository/model"
+	"github.com/google/uuid"
+	"time"
+)
+
+func SessionToServiceModel(data repoModel.Session) serviceModel.Session {
+	var updatedAt *time.Time
+	if data.UpdatedAt != nil {
+		tmp := time.Unix(*data.UpdatedAt, 0)
+		updatedAt = &tmp
+	}
+
+	return serviceModel.Session{
+		UserUUID:  uuid.MustParse(data.UserUUID),
+		CreatedAt: time.Unix(data.CreatedAt, 0),
+		UpdatedAt: updatedAt,
+		ExpiresAt: time.Unix(data.ExpiresAt, 0),
+	}
+}
