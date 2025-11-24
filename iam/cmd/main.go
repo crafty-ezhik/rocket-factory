d72e042218e2dbcff009c560f5770bd80d1f6e14
Alexandr Kotov <sa.cotov@yandex.ru>
2025-11-25 01:24:37 +0300

    feat: Добавлен DI контейнер и запуск сервиса

--- iam/cmd/main.go
+++ iam/cmd/main.go
@@ -3,30 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"net"
+	"github.com/crafty-ezhik/rocket-factory/iam/internal/app"
+	"github.com/crafty-ezhik/rocket-factory/platform/pkg/closer"
+	"go.uber.org/zap"
+	"os/signal"
+	"syscall"
 	"time"
 
-	redigo "github.com/gomodule/redigo/redis"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/reflection"
-
-	authAPIV1 "github.com/crafty-ezhik/rocket-factory/iam/internal/api/auth/v1"
-	userAPIV1 "github.com/crafty-ezhik/rocket-factory/iam/internal/api/user/v1"
 	"github.com/crafty-ezhik/rocket-factory/iam/internal/config"
-	"github.com/crafty-ezhik/rocket-factory/iam/internal/interceptor"
-	"github.com/crafty-ezhik/rocket-factory/iam/internal/repository/session"
-	"github.com/crafty-ezhik/rocket-factory/iam/internal/repository/user"
-	"github.com/crafty-ezhik/rocket-factory/iam/internal/service/auth"
-	serviceUser "github.com/crafty-ezhik/rocket-factory/iam/internal/service/user"
-	redisWrap "github.com/crafty-ezhik/rocket-factory/platform/pkg/cache/redis"
-	sharedIns "github.com/crafty-ezhik/rocket-factory/platform/pkg/grpc/interceptors"
-	"github.com/crafty-ezhik/rocket-factory/platform/pkg/hasher/bcrypt"
 	"github.com/crafty-ezhik/rocket-factory/platform/pkg/logger"
-	authV1 "github.com/crafty-ezhik/rocket-factory/shared/pkg/proto/auth/v1"
-	userV1 "github.com/crafty-ezhik/rocket-factory/shared/pkg/proto/user/v1"
 )
 
 const configPath = "../deploy/compose/iam/.env"
@@ -34,70 +19,33 @@ const configPath = "../deploy/compose/iam/.env"
 func main() {
 	err := config.Load(configPath)
 	if err != nil {
-		log.Println(err)
+		panic(fmt.Errorf("❌ Ошибка загрузки конфига: %w", err))
+	}
+
+	appCtx, appCancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer appCancel()
+	defer gracefulShutdown()
+
+	closer.Configure(syscall.SIGINT, syscall.SIGTERM)
+
+	a, err := app.New(appCtx)
+	if err != nil {
+		logger.Error(appCtx, "❌ Не удалось создать приложение", zap.Error(err))
 		return
 	}
-	ctx, cancel := context.WithCancel(context.Background())
+
+	err = a.Run(appCtx)
+	if err != nil {
+		logger.Error(appCtx, "❌ Ошибка при работе приложения", zap.Error(err))
+		return
+	}
+}
+
+func gracefulShutdown() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// === POSTGRES ===
-	pool, err := pgxpool.New(ctx, config.AppConfig().Postgres.URI())
-	if err != nil {
-		panic(fmt.Sprintf("❌ Ошибка подключения к базе данных: %v\n", err))
-	}
-
-	// Проверка соединения
-	pingCtx, pingCancel := context.WithTimeout(ctx, 5*time.Second)
-	defer pingCancel()
-
-	err = pool.Ping(pingCtx)
-	if err != nil {
-		panic(fmt.Sprintf("failed to ping Postgres: %v\n", err))
-	}
-
-	// === REDIS ===
-	redisPool := &redigo.Pool{
-		MaxIdle:     config.AppConfig().Redis.MaxIdle(),
-		IdleTimeout: config.AppConfig().Redis.IdleTimeout(),
-		DialContext: func(ctx context.Context) (redigo.Conn, error) {
-			return redigo.DialContext(ctx, "tcp", config.AppConfig().Redis.Address())
-		},
-	}
-
-	// === ЗАВИСИМОСТИ ===
-	redisClient := redisWrap.NewClient(redisPool, logger.Logger(), config.AppConfig().Redis.ConnectionTimeout())
-	hasher := bcrypt.NewBcryptPasswordHasher(10)
-
-	userRepo := user.NewRepository(pool)
-	sessionRepo := session.NewRepository(redisClient)
-	userService := serviceUser.NewService(userRepo, hasher)
-	authService := auth.NewService(userRepo, sessionRepo, hasher)
-	apiUser := userAPIV1.NewUserAPI(userService)
-	apiAuth := authAPIV1.NewAuthAPI(authService)
-
-	// === СЕРВЕР ===
-	server := grpc.NewServer(
-		grpc.Creds(insecure.NewCredentials()),
-		grpc.ChainUnaryInterceptor(
-			interceptor.LoggerInterceptor(),
-
-			sharedIns.UnaryErrorInterceptor(),
-		))
-
-	reflection.Register(server)
-
-	userV1.RegisterUserServiceServer(server, apiUser)
-	authV1.RegisterAuthServiceServer(server, apiAuth)
-
-	listener, err := net.Listen("tcp", config.AppConfig().IamGRPC.Address())
-	if err != nil {
-		log.Printf("failed to listen: %v", err)
-		return
-	}
-
-	log.Printf("🚀 grpc server listening at %v\n", listener.Addr())
-	if err := server.Serve(listener); err != nil {
-		log.Printf("failed to serve: %v", err)
-		return
+	if err := closer.CloseAll(ctx); err != nil {
+		logger.Error(ctx, "❌ Ошибка при завершении работы", zap.Error(err))
 	}
 }
--- iam/internal/app/app.go
+++ iam/internal/app/app.go
@@ -1,1 +1,125 @@
 package app
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/crafty-ezhik/rocket-factory/iam/internal/config"
+	"github.com/crafty-ezhik/rocket-factory/iam/internal/interceptor"
+	"github.com/crafty-ezhik/rocket-factory/platform/pkg/closer"
+	"github.com/crafty-ezhik/rocket-factory/platform/pkg/grpc/health"
+	"github.com/crafty-ezhik/rocket-factory/platform/pkg/logger"
+	authV1 "github.com/crafty-ezhik/rocket-factory/shared/pkg/proto/auth/v1"
+	userV1 "github.com/crafty-ezhik/rocket-factory/shared/pkg/proto/user/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+type App struct {
+	diContainer *diContainer
+	grpcServer  *grpc.Server
+	listener    net.Listener
+}
+
+func New(ctx context.Context) (*App, error) {
+	a := &App{}
+
+	err := a.initDeps(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+func (a *App) Run(ctx context.Context) error {
+	return a.runGRPCServer(ctx)
+}
+
+func (a *App) initDeps(ctx context.Context) error {
+	inits := []func(ctx context.Context) error{
+		a.initDI,
+		a.initLogger,
+		a.initCloser,
+		a.initListener,
+		a.initGRPCServer,
+	}
+
+	for _, f := range inits {
+		err := f(ctx)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
+
+func (a *App) initDI(_ context.Context) error {
+	a.diContainer = NewDIContainer()
+	return nil
+}
+
+func (a *App) initLogger(_ context.Context) error {
+	return logger.Init(
+		config.AppConfig().Logger.Level(),
+		config.AppConfig().Logger.AsJSON(),
+	)
+}
+
+func (a *App) initCloser(_ context.Context) error {
+	closer.SetLogger(logger.Logger())
+	return nil
+}
+
+func (a *App) initListener(_ context.Context) error {
+	listener, err := net.Listen("tcp", config.AppConfig().IamGRPC.Address())
+	if err != nil {
+		return err
+	}
+	closer.AddNamed("TCP listener", func(ctx context.Context) error {
+		lerr := listener.Close()
+		if lerr != nil && !errors.Is(lerr, net.ErrClosed) {
+			return err
+		}
+		return nil
+	})
+
+	a.listener = listener
+
+	return nil
+}
+
+func (a *App) initGRPCServer(ctx context.Context) error {
+	a.grpcServer = grpc.NewServer(
+		grpc.Creds(insecure.NewCredentials()),
+		grpc.ChainUnaryInterceptor(
+			interceptor.LoggerInterceptor(),
+		),
+	)
+
+	closer.AddNamed("IAM GRPC Server", func(ctx context.Context) error {
+		a.grpcServer.GracefulStop()
+		return nil
+	})
+
+	reflection.Register(a.grpcServer)
+
+	// Регистрируем health service для проверки работоспособности
+	health.RegisterService(a.grpcServer)
+
+	// Регистрируем хендлеры
+	authV1.RegisterAuthServiceServer(a.grpcServer, a.diContainer.AuthV1API(ctx))
+	userV1.RegisterUserServiceServer(a.grpcServer, a.diContainer.UserV1API(ctx))
+
+	return nil
+}
+
+func (a *App) runGRPCServer(ctx context.Context) error {
+	logger.Info(ctx, fmt.Sprintf("🚀 gRPC InventoryService server listening on %s", config.AppConfig().IamGRPC.Address()))
+	if err := a.grpcServer.Serve(a.listener); err != nil {
+		return err
+	}
+	return nil
+}
